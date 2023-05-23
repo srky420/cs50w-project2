c5ba682d773cdc42ec319ad8b8c8772750454778
srky <srky420@gmail.com>
2023-05-24 00:04:45 +0500

    Create optimizations and tabs

--- auctions/static/auctions/script.js
+++ auctions/static/auctions/script.js
@@ -1,11 +1,40 @@
 // For bids list in listing page
-const bidlistBtn = document.getElementById('bidlist-btn');
+const bidlistBtn = document.querySelector('#bidlist-btn');
 const bidlist = document.querySelector('#bidlist');
 
-bidlistBtn.addEventListener('click', function(e) {
-    bidlist.classList.toggle('visually-hidden');
-})
+if (bidlistBtn) {
 
+    bidlistBtn.addEventListener('click', function(e) {
+        bidlist.classList.toggle('visually-hidden');
+    });
+}
+
+// For tabs
+const tab1 = document.querySelector('#tab-1');
+const tab2 = document.querySelector('#tab-2');
+
+const tabItem1 = document.querySelector('#tab-item-1');
+const tabItem2 = document.querySelector('#tab-item-2');
+
+tab1.addEventListener('click', function(e) {
+    if (!tab1.classList.contains('active')) {
+        tab1.classList.toggle('active');
+        tab2.classList.toggle('active');
+        tabItem1.classList.toggle('visually-hidden');
+        tabItem2.classList.toggle('visually-hidden');
+    }
+
+});
+
+tab2.addEventListener('click', function(e) {
+    if (!tab2.classList.contains('active')) {
+        tab1.classList.toggle('active')
+        tab2.classList.toggle('active');
+        tabItem1.classList.toggle('visually-hidden');
+        tabItem2.classList.toggle('visually-hidden');
+    }
+
+});
 
 
 
